7e5d9c0696145c3ae47c6cffea1889406398f07b
Máté <mate.lorinczi97@gmail.com>
2023-05-09 14:02:58 +0200

    Init project backend

---
+++ MeloMeet-backend/src/main/java/com/codecool/melomeetbackend/MeloMeetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.melomeetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeloMeetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeloMeetBackendApplication.class, args);
+	}
+
+}
---
+++ MeloMeet-backend/src/test/java/com/codecool/melomeetbackend/MeloMeetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.melomeetbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeloMeetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
