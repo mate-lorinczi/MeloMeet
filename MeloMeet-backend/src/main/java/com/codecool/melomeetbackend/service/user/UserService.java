460b4d7156465b3bd9b911b0f4037f225b467019
Máté <mate.lorinczi97@gmail.com>
2023-05-15 22:13:23 +0200

    Add user service

--- MeloMeet-backend/src/main/java/com/codecool/melomeetbackend/service/user/UserService.java
+++ MeloMeet-backend/src/main/java/com/codecool/melomeetbackend/service/user/UserService.java
@@ -1,8 +1,16 @@
 package com.codecool.melomeetbackend.service.user;
 
 import com.codecool.melomeetbackend.dto.user.NewUserDTO;
+import com.codecool.melomeetbackend.dto.user.UserDTO;
+
+import java.util.Set;
 
 public interface UserService {
 
     UserDTO addNewUser(NewUserDTO newUserDTO);
+    UserDTO getUserByUserId(String userId);
+    Set<UserDTO> getFriendsByUserId(String userId);
+    UserDTO updateUser(UserDTO userDTO);
+    boolean addFriend(String friendRequestSenderId, String friendRequestReceiverId);
+    UserDTO getUserByUsername(String username);
 }
