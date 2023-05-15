c996a51c89d6ead39cec72b13e5b06cf0e252a90
Máté <mate.lorinczi97@gmail.com>
2023-05-15 23:02:34 +0200

    Add method for posting new user in user service

--- MeloMeet-backend/src/main/java/com/codecool/melomeetbackend/service/user/UserService.java
+++ MeloMeet-backend/src/main/java/com/codecool/melomeetbackend/service/user/UserService.java
@@ -2,12 +2,13 @@ package com.codecool.melomeetbackend.service.user;
 
 import com.codecool.melomeetbackend.dto.user.NewUserDTO;
 import com.codecool.melomeetbackend.dto.user.UserDTO;
+import com.codecool.melomeetbackend.utility.excepiton.UserRegistrationException;
 
 import java.util.Set;
 
 public interface UserService {
 
-    UserDTO addNewUser(NewUserDTO newUserDTO);
+    UserDTO addNewUser(NewUserDTO newUserDTO) throws UserRegistrationException;
     UserDTO getUserByUserId(String userId);
     Set<UserDTO> getFriendsByUserId(String userId);
     UserDTO updateUser(UserDTO userDTO);
--- MeloMeet-backend/src/main/java/com/codecool/melomeetbackend/service/user/UserServiceImpl.java
+++ MeloMeet-backend/src/main/java/com/codecool/melomeetbackend/service/user/UserServiceImpl.java
@@ -2,15 +2,44 @@ package com.codecool.melomeetbackend.service.user;
 
 import com.codecool.melomeetbackend.dto.user.NewUserDTO;
 import com.codecool.melomeetbackend.dto.user.UserDTO;
+import com.codecool.melomeetbackend.model.User;
+import com.codecool.melomeetbackend.repository.UserRepository;
+import com.codecool.melomeetbackend.utility.excepiton.UserRegistrationException;
+import com.codecool.melomeetbackend.utility.mappers.userMapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class UserServiceImpl implements UserService {
 
+    private final UserMapper userMapper;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserMapper userMapper, UserRepository userRepository) {
+        this.userMapper = userMapper;
+        this.userRepository = userRepository;
+    }
+
     @Override
-    public UserDTO addNewUser(NewUserDTO newUserDTO) {
-        return null;
+    public UserDTO addNewUser(NewUserDTO newUserDTO) throws UserRegistrationException {
+
+        UserDTO userDTO;
+
+        try {
+            User newUser = userMapper.newUserDTOToUser(newUserDTO);
+            User savedUser = userRepository.save(newUser);
+
+            userDTO = userMapper.userEntityToUserDTO(savedUser);
+
+        } catch (UserRegistrationException e) {
+            throw e;
+        }
+
+        return userDTO;
     }
 
     @Override
