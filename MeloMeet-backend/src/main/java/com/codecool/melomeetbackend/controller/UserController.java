baa10a1ce052d7ce2e30bb0c876997cb898294fb
Máté <mate.lorinczi97@gmail.com>
2023-05-16 17:12:08 +0200

    Add request for registrating a new user on user service

---
+++ MeloMeet-backend/src/main/java/com/codecool/melomeetbackend/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.codecool.melomeetbackend.controller;
+
+import com.codecool.melomeetbackend.dto.user.NewUserDTO;
+import com.codecool.melomeetbackend.dto.user.UserDTO;
+import com.codecool.melomeetbackend.service.user.UserService;
+import com.codecool.melomeetbackend.utility.excepiton.UserRegistrationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> addNewUser(@RequestBody NewUserDTO newUserDTO) {
+
+        try {
+            UserDTO addedUser = userService.addNewUser(newUserDTO);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(addedUser);
+        } catch (UserRegistrationException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        }
+    }
+}
