04836b165adaf75ce12ddeeaced61196f32407d4
Máté <mate.lorinczi97@gmail.com>
2023-05-16 22:36:07 +0200

    Add interface for group service

--- MeloMeet-backend/src/main/java/com/codecool/melomeetbackend/service/group/GroupService.java
+++ MeloMeet-backend/src/main/java/com/codecool/melomeetbackend/service/group/GroupService.java
@@ -1,6 +1,7 @@
 package com.codecool.melomeetbackend.service.group;
 
 import com.codecool.melomeetbackend.dto.events.ConcertEventDTO;
+import com.codecool.melomeetbackend.dto.events.EventDTO;
 import com.codecool.melomeetbackend.dto.group.GroupDTO;
 import com.codecool.melomeetbackend.dto.group.NewGroupDTO;
 import com.codecool.melomeetbackend.model.eventModel.ConcertEvent;
@@ -8,5 +9,10 @@ import org.springframework.stereotype.Service;
 
 public interface GroupService {
 
-    GroupDTO<ConcertEventDTO> addNewConcertGroup(NewGroupDTO newGroupDTO);
+    GroupDTO<ConcertEventDTO> addNewGroupForAConcertEvent(NewGroupDTO newGroupDTO);
+    GroupDTO<ConcertEventDTO> getAllOpenGroups();
+    GroupDTO<ConcertEventDTO> getAllOpenGroupsByConcertEventId(String eventId);
+    GroupDTO<EventDTO> getAllInvitedGroupsByUserId(String userId);
+    
+
 }
