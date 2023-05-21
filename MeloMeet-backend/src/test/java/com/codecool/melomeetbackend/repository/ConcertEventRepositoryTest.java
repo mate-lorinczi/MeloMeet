e1efef285acbaccff0025dca5411c639ca691f33
Máté <mate.lorinczi97@gmail.com>
2023-05-21 22:42:07 +0200

    Add tests for concert event entity and concert event mapper

---
+++ MeloMeet-backend/src/test/java/com/codecool/melomeetbackend/repository/ConcertEventRepositoryTest.java
@@ -0,0 +1,91 @@
+package com.codecool.melomeetbackend.repository;
+
+import com.codecool.melomeetbackend.model.Performer;
+import com.codecool.melomeetbackend.model.User;
+import com.codecool.melomeetbackend.model.Venue;
+import com.codecool.melomeetbackend.model.eventModel.ConcertEvent;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.util.Set;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringJUnitConfig
+@DataJpaTest
+class ConcertEventRepositoryTest {
+
+    @Autowired
+    private ConcertEventRepository concertEventRepository;
+    @Autowired
+    private VenueRepository venueRepository;
+    @Autowired
+    private PerformerRepository performerRepository;
+    @Autowired
+    private UserRepository userRepository;
+    private User user;
+    private Performer performer;
+    private Venue venue;
+
+    @BeforeEach
+    void setUp() {
+        concertEventRepository.deleteAll();
+        venueRepository.deleteAll();
+        performerRepository.deleteAll();
+        userRepository.deleteAll();
+
+        this.user = User.builder().password("password").email("email").username("username").build();
+        userRepository.save(user);
+
+        this.performer = new Performer();
+        performerRepository.save(performer);
+
+        this.venue = new Venue();
+        venueRepository.save(venue);
+    }
+
+    @Test
+    void testUUIDGeneration() {
+        ConcertEvent concertEvent =
+                ConcertEvent.builder().createdBy(user).performers(Set.of(performer)).venue(venue).build();
+
+        concertEventRepository.save(concertEvent);
+
+        System.out.println(concertEvent.getEventId());
+
+        assertTrue(isValidUUID(concertEvent.getEventId()));
+
+    }
+
+    @Test
+    void testNonNulls() {
+
+        ConcertEvent concertEvent =
+                ConcertEvent.builder().createdBy(user).performers(Set.of(performer)).venue(venue).build();
+
+        ConcertEvent savedConcertEvent = concertEventRepository.save(concertEvent);
+
+        assertAll("Grouped assertions of non nulls of ConcertEvent",
+                () -> assertNotNull(savedConcertEvent.getEventId()),
+                () -> assertNotNull(savedConcertEvent.getCreatedBy()),
+                () -> assertNotNull(savedConcertEvent.getPerformers()),
+                () -> assertNotNull(savedConcertEvent.getVenue())
+                );
+    }
+
+    private boolean isValidUUID(UUID uuid) {
+        try {
+            UUID.fromString(uuid.toString());
+
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
---
+++ MeloMeet-backend/src/test/java/com/codecool/melomeetbackend/utility/mappers/eventMapper/ConcertEventMapperTest.java
@@ -0,0 +1,81 @@
+package com.codecool.melomeetbackend.utility.mappers.eventMapper;
+
+import com.codecool.melomeetbackend.model.Performer;
+import com.codecool.melomeetbackend.model.Style;
+import com.codecool.melomeetbackend.model.User;
+import com.codecool.melomeetbackend.model.eventModel.ConcertEvent;
+import com.codecool.melomeetbackend.repository.PerformerRepository;
+import com.codecool.melomeetbackend.repository.StyleRepository;
+import com.codecool.melomeetbackend.repository.UserRepository;
+import com.codecool.melomeetbackend.service.dto.events.ConcertEventDTO;
+import com.codecool.melomeetbackend.service.dto.user.UserDTO;
+import com.codecool.melomeetbackend.utility.mappers.userMapper.UserMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+class ConcertEventMapperTest {
+
+    @Autowired
+    private EventMapper eventMapper;
+    @Mock
+    private PerformerRepository performerRepository;
+    @Mock
+    private StyleRepository styleRepository;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private UserMapper userMapper;
+    private UserDTO userDTO;
+
+    @BeforeEach
+    void setUp() {
+        this.userDTO = new UserDTO("userId", "username", "email",
+                Timestamp.valueOf(LocalDateTime.now()), false, false);
+    }
+
+    @Test
+    void testEventMapperCorrectlyMapsEntityToDTO() {
+        UUID id = UUID.randomUUID();
+        LocalDateTime startDate = LocalDateTime.now();
+        LocalDateTime endDate = startDate.plusDays(1);
+        Performer performer = new Performer();
+        Set<Performer> performerSet = Set.of(performer);
+        Style style = new Style();
+        Set<Style> styleSet = Set.of(style);
+        User user = User.builder().userID(UUID.randomUUID()).build();
+
+
+        //when(userMapper.userEntityToUserDTO(any())).thenReturn(this.userDTO);
+
+        ConcertEvent concertEvent =
+                ConcertEvent.builder().eventId(id).startDateAndTime(startDate).endDateAndTime(endDate).performers(performerSet).styles(styleSet).createdBy(user).build();
+
+        ConcertEventDTO eventDTO = eventMapper.mapConcertEventToConcertEventDTO(concertEvent);
+
+        assertAll(
+                () -> assertEquals(concertEvent.getEventId().toString(), eventDTO.eventId()),
+                () -> assertTrue(concertEvent.getEndDateAndTime().equals(eventDTO.endDateAndTime()))
+        );
+    }
+
+}
