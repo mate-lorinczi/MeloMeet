cb9ae48d7966a5413c9eaac43d1c0de1d0327b70
Máté <mate.lorinczi97@gmail.com>
2023-05-24 19:25:28 +0200

    Add tests for find by id

--- MeloMeet-backend/src/test/java/com/codecool/melomeetbackend/service/venue/VenueServiceImpTest.java
+++ MeloMeet-backend/src/test/java/com/codecool/melomeetbackend/service/venue/VenueServiceImpTest.java
@@ -8,6 +8,7 @@ import com.codecool.melomeetbackend.service.dto.venue.WholeAddress;
 import com.codecool.melomeetbackend.utility.mappers.venueMapper.VenueMapper;
 import com.codecool.melomeetbackend.utility.mappers.venueMapper.VenueMapperImpl;
 import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -17,6 +18,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -33,19 +35,20 @@ class VenueServiceImpTest {
     private VenueDTO venueDTO;
     private NewVenueDTO newVenueDTO;
     private Venue venue;
+    private Set<VenueDTO> venueDTOSet;
 
     @BeforeEach
     void setUp() {
         venueRepository.deleteAll();
 
-        String venueID = "6c77ea0c-3bc8-4371-9a21-43b799de0f92";
+        String venueID1 = "6c77ea0c-3bc8-4371-9a21-43b799de0f92";
         String name = "Test Name";
         WholeAddress wholeAddress = new WholeAddress("Budapest", "5600", "Alkotás Utca 26");
         newVenueDTO = new NewVenueDTO(name, wholeAddress, true);
-        venueDTO = new VenueDTO(name, wholeAddress, true, venueID);
+        venueDTO = new VenueDTO(name, wholeAddress, true, venueID1);
 
         venue =
-                Venue.builder().venueId(UUID.fromString(venueID)).city(wholeAddress.city()).postalCode(wholeAddress.postalCode()).address(wholeAddress.address()).isOpenAir(true).build();
+                Venue.builder().venueId(UUID.fromString(venueID1)).city(wholeAddress.city()).postalCode(wholeAddress.postalCode()).address(wholeAddress.address()).isOpenAir(true).build();
     }
 
     @Test
@@ -53,19 +56,23 @@ class VenueServiceImpTest {
 
         Mockito.when(venueRepository.save(venue)).thenReturn(venue);
         Mockito.when(venueMapper.venueToVenueDTO(venue)).thenReturn(venueDTO);
+        Mockito.when(venueMapper.newVenueDTOToVenue(newVenueDTO)).thenReturn(venue);
         Mockito.when(venueRepository.existsByAddressAndCityAndPostalCode(Mockito.any(),
                 Mockito.any(), Mockito.any())).thenReturn(false);
 
         VenueDTO result = venueService.addNewVenue(newVenueDTO);
 
-        assertAll(() -> assertEquals("Test Name", venueDTO.name()),
-                () -> assertEquals("Budapest", venueDTO.wholeAddress().city()),
-                () -> assertEquals("5600", venueDTO.wholeAddress().postalCode()),
-                () -> assertEquals("Alkotás Utca 26", venueDTO.wholeAddress().address()),
+        assertAll(() -> assertEquals("Test Name", result.name()),
+                () -> assertEquals("Budapest", result.wholeAddress().city()),
+                () -> assertEquals("5600", result.wholeAddress().postalCode()),
+                () -> assertEquals("Alkotás Utca 26", result.wholeAddress().address()),
                 () -> assertEquals("6c77ea0c-3bc8-4371-9a21-43b799de0f92",
-                        venueDTO.venueId()),
-                () -> assertTrue(venueDTO.isOpenAir())
+                        result.venueId()),
+                () -> assertTrue(result.isOpenAir())
                 );
+        Mockito.verify(venueRepository, Mockito.times(1)).save(Mockito.any());
+        Mockito.verify(venueMapper, Mockito.times(1)).venueToVenueDTO(Mockito.any());
+        Mockito.verify(venueRepository, Mockito.times(1)).existsByAddressAndCityAndPostalCode(Mockito.any(), Mockito.any(), Mockito.any());
     }
 
     @Test
@@ -77,4 +84,39 @@ class VenueServiceImpTest {
         EntityExistsException entityExistsException = assertThrows(EntityExistsException.class, () -> venueService.addNewVenue(newVenueDTO));
         assertEquals("Venue with the same address already exists!", entityExistsException.getMessage());
     }
+
+    @Test
+    void testFindByIdWithCorrectId() {
+
+        Mockito.when(venueRepository.findById(UUID.fromString("6c77ea0c-3bc8-4371-9a21" +
+                "-43b799de0f92"))).thenReturn(Optional.of(venue));
+        Mockito.when(venueMapper.venueToVenueDTO(venue)).thenReturn(venueDTO);
+
+        var result = venueService.findById("6c77ea0c-3bc8-4371-9a21" +
+                "-43b799de0f92");
+
+        assertAll(() -> assertEquals("Test Name", result.name()),
+                () -> assertEquals("Budapest", result.wholeAddress().city()),
+                () -> assertEquals("5600", result.wholeAddress().postalCode()),
+                () -> assertEquals("Alkotás Utca 26", result.wholeAddress().address()),
+                () -> assertEquals("6c77ea0c-3bc8-4371-9a21-43b799de0f92",
+                        result.venueId()),
+                () -> assertTrue(result.isOpenAir())
+        );
+    }
+
+    @Test
+    void testFindByIdWithIncorrectId() {
+
+        String randomId = UUID.randomUUID().toString();
+        String msg = "Venue with id " + randomId + " not found!";
+        Mockito.when(venueRepository.findById(Mockito.any())).thenReturn(Optional.empty());
+        EntityNotFoundException entityNotFoundException =
+                assertThrows(EntityNotFoundException.class,
+                () -> venueService.findById(randomId));
+
+        assertEquals(msg, entityNotFoundException.getMessage());
+    }
+
+
 }
