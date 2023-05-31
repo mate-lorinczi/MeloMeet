ce9bb685bf7282496b25359d06138da7cd689b05
Máté <mate.lorinczi97@gmail.com>
2023-05-31 10:44:40 +0200

    Add bycryptpasswordencoder bean

--- MeloMeet-backend/src/main/java/com/codecool/melomeetbackend/service/CustomUserDetailsService.java
+++ MeloMeet-backend/src/main/java/com/codecool/melomeetbackend/service/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package com.codecool.melomeetbackend.service;
 
 import com.codecool.melomeetbackend.model.User;
 import com.codecool.melomeetbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,7 +15,10 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
