a425055b4658707ee822555a66f4d8361d3c4b22
Máté <mate.lorinczi97@gmail.com>
2023-05-31 10:52:25 +0200

    Add dao auth provider

--- MeloMeet-backend/src/main/java/com/codecool/melomeetbackend/security/SecurityConfig.java
+++ MeloMeet-backend/src/main/java/com/codecool/melomeetbackend/security/SecurityConfig.java
@@ -2,8 +2,12 @@ package com.codecool.melomeetbackend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,6 +16,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
@@ -19,7 +29,15 @@ public class SecurityConfig {
 
         return httpSecurity.build();
     }
-    
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+
+        return new ProviderManager(daoAuthenticationProvider);
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
