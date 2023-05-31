4bb303f92f3ed80b9db31223ab47cb8e5dc78cb5
Máté <mate.lorinczi97@gmail.com>
2023-05-31 10:21:18 +0200

    Add custom user details

--- MeloMeet-backend/src/main/java/com/codecool/melomeetbackend/service/CustomUserDetailsService.java
+++ MeloMeet-backend/src/main/java/com/codecool/melomeetbackend/service/CustomUserDetailsService.java
@@ -1,5 +1,9 @@
 package com.codecool.melomeetbackend.service;
 
+import com.codecool.melomeetbackend.model.User;
+import com.codecool.melomeetbackend.repository.UserRepository;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,8 +12,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+    private final UserRepository userRepository;
+
+
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user =
+                userRepository.findByUsername(username).orElseThrow(() -> new BadCredentialsException("Username not found!"));
+        UserDetails userDetails = org.springframework.security.core.userdetails.User.withUsername(username).password(user.getPassword()).build();
+
+        return userDetails;
     }
 }
