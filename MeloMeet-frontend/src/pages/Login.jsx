01e387e87d4f3579a8a9a016011dc82e35f2461a
Máté <mate.lorinczi97@gmail.com>
2023-06-01 17:55:28 +0200

    Sett localstorage with userId on login

--- MeloMeet-frontend/src/pages/Login.jsx
+++ MeloMeet-frontend/src/pages/Login.jsx
@@ -1,34 +1,48 @@
+import { useState } from "react";
 import UsernamePasswordFields from "../components/UsernamePassword";
 
-const postLogin = async(loginData) => {
-    const res = await fetch('/api/users/login', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(loginData)
-    })
+const postLogin = async (loginData, setLoginMessage) => {
+  const res = await fetch("/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(loginData),
+  });
 
-    if(!res.ok) {
-        console.log(res);
-    }
-    const id = await res.text();
-    console.log(id);
+  if (!res.ok) {
+    return setLoginMessage("Username or password is invalid!");
+  }
 
-    return id;
-}
+  //TODO: Login - rework setting userId in localstorage!
+
+  const id = await res.text();
+  window.localStorage.setItem("userId", id);
+  setLoginMessage("Successfully logged in!");
+
+  return id;
+};
 
 const Login = () => {
-    const SUBMIT_NAME = "Login";
-    const handleLogin = async(loginData) => {
-        const res = await postLogin(loginData);
+  const SUBMIT_NAME = "Login";
+  const [loginMessage, setLoginMessage] = useState(null);
 
-        console.log(res);
+  const handleLogin = async (loginData) => {
+    if (window.localStorage.getItem("userId")) {
+      return setLoginMessage("Already logged in!");
     }
 
-    return ( 
-        <UsernamePasswordFields submitName={SUBMIT_NAME} submit={handleLogin}/>
-     );
-}
- 
+    const res = await postLogin(loginData, setLoginMessage);
+
+    console.log(res)
+  };
+
+  return (
+    <div>
+      <UsernamePasswordFields submitName={SUBMIT_NAME} submit={handleLogin} />
+      {loginMessage ? <div>{loginMessage}</div> : loginMessage}
+    </div>
+  );
+};
+
 export default Login;
