be81d6cafec44ce85fb46822f0362bb5ee09bc1c
Máté <mate.lorinczi97@gmail.com>
2023-06-02 17:32:59 +0200

    Add logut button when logged in

--- MeloMeet-frontend/src/pages/Login.jsx
+++ MeloMeet-frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import UsernamePasswordFields from "../components/UsernamePassword";
 
-const postLogin = async (loginData, setLoginMessage) => {
+const postLogin = async (loginData, setLoginMessage, setUserId) => {
   const res = await fetch("/api/users/login", {
     method: "POST",
     headers: {
@@ -19,27 +19,35 @@ const postLogin = async (loginData, setLoginMessage) => {
   const id = await res.text();
   window.localStorage.setItem("userId", id);
   setLoginMessage("Successfully logged in!");
-
+  setUserId(id);
   return id;
 };
 
 const Login = () => {
+
   const SUBMIT_NAME = "Login";
   const [loginMessage, setLoginMessage] = useState(null);
+  const [userId, setUserId] = useState(window.localStorage.getItem("userId"));
 
   const handleLogin = async (loginData) => {
     if (window.localStorage.getItem("userId")) {
       return setLoginMessage("Already logged in!");
     }
 
-    const res = await postLogin(loginData, setLoginMessage);
+    const res = await postLogin(loginData, setLoginMessage, setUserId);
 
     console.log(res)
   };
 
+  const handleLogout = () => {
+    window.localStorage.removeItem("userId");
+    setUserId(null);
+  }
+
   return (
     <div>
       <UsernamePasswordFields submitName={SUBMIT_NAME} submit={handleLogin} />
+      {userId ? <button onClick={handleLogout}>Logout</button> : null}
       {loginMessage ? <div>{loginMessage}</div> : loginMessage}
     </div>
   );
