831642d3a9332fb97f0e3e4119db2ffa882d067a
Máté <mate.lorinczi97@gmail.com>
2023-06-05 15:54:37 +0200

    Add concert form component

---
+++ MeloMeet-backend/src/main/resources/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO style (name)
+VALUES
+    ('Jazz'),
+    ('Pop'),
+    ('Hip Hop'),
+    ('Rock'),
+    ('Indie'),
+    ('Magyar Alternatív'),
+    ('Metál'),
+    ('Electronic'),
+    ('Folk'),
+    ('Blues'),
+    ('Punk'),
+    ('Dance'),
+    ('R&B'),
+    ('Techno'),
+    ('House'),
+    ('Psychedelic Rock'),
+    ('Classical');
---
+++ MeloMeet-frontend/src/components/ConcertForm.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+const ConcertForm = (props) => {
+
+  const {styles} = props;
+  const [values, setValues] = useState({
+    "startDateAndTime" : "",
+    "endDateAndTime" : "",
+    "performers" : [],
+    "styles" : [],
+    "currPerformer" : "",
+    "currStyle": ""
+  });
+
+  const addPerformer = () => {
+    const newPerformer = [...values.currPerformer];
+    newPerformer.push(values.currPerformer)
+    setValues({...values, "performers": newPerformer, "currPerformer" : ""})
+  }
+
+  const addStyle = () => {
+    const newStyle = [...values.currPerformer];
+    newStyle.push(values.currStyle)
+    setValues({...values, "styles": newStyle, "currStyle" : ""})
+  }
+
+  const handleChange = (prop) => (event) => {
+    const newValues = {...values, [prop] : event.target.value};
+    setValues(newValues);
+  }
+
+  return (
+    <div>
+      <div className="startTime">
+        <label htmlFor="startTime">Choose start time:</label>
+        <input type="datetime-local" name="startTime" onChange={handleChange("startTime")}/>
+      </div>
+      <div className="endTime">
+        <label htmlFor="startTime">Choose end time:</label>
+        <input type="datetime-local" name="endTime" onChange={handleChange("endTime")}/>
+      </div>
+      <div className="performer">
+        <label htmlFor="performer">Add performer:</label>
+        <input type="text" name="performer" onChange={handleChange("currPerformer")}/>
+        <button onClick={addPerformer()}>Add</button>
+      </div>
+      <div className="styles">
+        <label htmlFor="styles">Add a style: </label>
+        <select name={styles} id="style-select" onClick={handleChange("currStyle")}>
+            <option value="">Please choose an option</option>
+            {styles.map(style => <option key={style.styleId} value={style.name}>{style.name}</option>)}
+        </select>
+        <button onClick={addStyle()}>Add Style</button>
+      </div>
+    </div>
+  );
+};
+
+export default ConcertForm;
