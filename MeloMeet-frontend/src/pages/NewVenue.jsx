369c1c2a4dfd6385334dabfdc4d19ac096e4c371
Máté <mate.lorinczi97@gmail.com>
2023-06-05 14:32:14 +0200

    Add response texts for new venue page

--- MeloMeet-frontend/src/pages/NewVenue.jsx
+++ MeloMeet-frontend/src/pages/NewVenue.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import VenueForm from "../components/VenueForm";
 
 const postNewVenue = async (values) => {
@@ -11,23 +12,32 @@ const postNewVenue = async (values) => {
 
     if(!res.ok) {
         console.log(res);
+        return res.status;
 
     }
 
-    return await res.json();
+    console.log(await res.json())
+    return res.status;
 }
 
 const NewVenue = () => {
 
+    const statusMap = {
+        201: "Venue added!",
+        409: "Venue already added, or bad address!"
+    }
+    const [statusText, setStatusText] = useState(null)
+
     const handleSubmit = async (values) => {
 
         const res = await postNewVenue(values);
-
-        console.log(res);
+        
+        setStatusText(statusMap[res]);
     }
 
     return ( <div>
         <VenueForm submit={handleSubmit} />
+        {statusText ? <div>{statusText}</div> : null}
     </div> );
 }
  
